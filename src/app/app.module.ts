b74dc95f72e1d6a2d5ba018ffee24403aea283ae
khaphan-github <76431966+khaphan-github@users.noreply.github.com>
2023-03-09 08:01:51 +0700

    Finish hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      HeroComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
+
+  exports: [RouterModule],
+  
   providers: [],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
